ad02f3af96261955b6fc3d4b0c85873fe27c7716
TomasMacoun <macount@seznam.cz>
2023-08-12 10:47:22 +0200

    initial commit of the project

---
+++ backend/src/main/java/com/gfa/foxbook/foxbook/FoxbookApplication.java
@@ -0,0 +1,13 @@
+package com.gfa.foxbook.foxbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxbookApplication.class, args);
+	}
+
+}
